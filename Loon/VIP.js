7c46e72f5eb10ac4da7b11c7370804dcae403daa
aenrus <47635135+aenrus@users.noreply.github.com>
2025-02-22 15:20:37 +0800

    Update VIP.js

--- Loon/VIP.js
+++ Loon/VIP.js
@@ -1,8 +1,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes.js
-
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/aenrus/Script/refs/heads/main/Loon/VIP.js
 [mitm]
 hostname = buy.itunes.apple.com
 
