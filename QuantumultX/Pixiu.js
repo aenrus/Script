543b7ebe006b40c0ca206258fc7c3b391eb6c40d
aenrus <47635135+aenrus@users.noreply.github.com>
2025-05-17 10:36:20 +0800

    Create Pixiu.js

---
+++ QuantumultX/Pixiu.js
@@ -0,0 +1,14 @@
+[rewrite_local]
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes.js
+
+[mitm]
+hostname = buy.itunes.apple.com
+
+const ddm = JSON.parse($response.body);
+const ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
+const bundle_id = ddm.receipt["bundle_id"] || ddm.receipt["Bundle_Id"];
+const yearid = `${bundle_id}.year`;
+const yearlyid = `${bundle_id}.yearly`;
+const yearlysubscription = `${bundle_id}.yearlysubscription`;
+const lifetimeid = `${bundle_id}.lifetime`;
+const list = {'com.RuoG.Pixiu': { cm: 'timea', hx: 'hxpda', id: "com.RuoG.Pixiu.VIPYear", latest: "ddm1023" }};
