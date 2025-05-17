44262dcc5a776fa204984b348c4fd2e13f951006
aenrus <47635135+aenrus@users.noreply.github.com>
2025-05-17 10:37:18 +0800

    Update Pixiu.js

--- QuantumultX/Pixiu.js
+++ QuantumultX/Pixiu.js
@@ -1,5 +1,5 @@
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/aenrus/Script/refs/heads/main/QuantumultX/Pixiu.js
 
 [mitm]
 hostname = buy.itunes.apple.com
